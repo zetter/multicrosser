ef19f7262b530ed1d3ceb07901981a3072574d84
Chris Zetter <zetter@users.noreply.github.com>
2018-11-27 14:51:51 +0000

    Send moves when reconnect
    This is a first attempt to record moves when a connection drops so they 
    can be resent later.
    
    This only records and resends all moves. It still looses moves if page 
    refreshed (this could be done by using local storage rather than session 
    storage, but I wanted to think about expiring before I did this) and it 
    does not handle conflicts well (last to send always wins).

---
+++ app/javascript/move_buffer.js
@@ -0,0 +1,28 @@
+class MoveBuffer {
+  constructor(key) {
+    this.storage = window.sessionStorage;
+    this.key = `move-buffer-${key}`;
+  }
+
+  queue(move) {
+    const existingItems = this.getObject();
+    existingItems.push(move);
+    this.setObject(existingItems);
+  }
+
+  deqeueAll() {
+    const existingItems = this.getObject();
+    this.setObject([]);
+    return existingItems;
+  }
+
+  setObject(object) {
+    this.storage.setItem(this.key, JSON.stringify(object));
+  }
+
+  getObject() {
+    return JSON.parse(this.storage.getItem(this.key)) || [];
+  }
+}
+
+export default MoveBuffer;
--- app/javascript/subscription.js
+++ app/javascript/subscription.js
@@ -1,7 +1,9 @@
 import ActionCable from 'actioncable';
+import MoveBuffer from 'move_buffer';
 
 const createSubscription = function createSubscription(crossword, room, onReceiveMove, onInitialState) {
   const cable = ActionCable.createConsumer(process.env.WEBSOCKET_URL);
+  const moveBuffer = new MoveBuffer(room);
 
   return cable.subscriptions.create(
     { channel: 'MovesChannel', crossword, room },
@@ -14,9 +16,16 @@ const createSubscription = function createSubscription(crossword, room, onReceiv
         }
       },
       move: function move(data) {
-        this.perform('move', data);
+        if (this.consumer.connection.isActive()) {
+          this.perform('move', data);
+        } else {
+          moveBuffer.queue(data);
+        }
       },
       connected: function connected() {
+        moveBuffer.deqeueAll().forEach((move) => {
+          this.move(move);
+        });
       },
     },
   );
